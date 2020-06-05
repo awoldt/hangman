ed7d1b35956d81fe1860af35df31697127f4bd2b
awoldt <48081351+awoldt@users.noreply.github.com>
2020-06-05 19:36:03 -0400

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,41 +1,29 @@
 import random
 
-phrases = ["cat"]
-censoredPhrase = ""
+phrases = ["apple"]
+censoredPhrase = "" #word with all characters replaced with * (ex: cat = ***)
+guesses = []
 
 #index of the random phrase to be picked
 index = random.randrange(0,len(phrases))
 #phrase the user has to guess
 randomPhrase = phrases[index]
-
 #generates the censored str
 for i in range(0, len(randomPhrase)):
     censoredPhrase += "*"
-print(censoredPhrase)
 
-userStr = ""
-index = 0
-correctGuess = False
-while(correctGuess == False):
-    #until user can guess correct char
-    loop = True
-    while(loop == True):
-        if(len(userStr) != len(randomPhrase)):
-            print(userStr)
-            userGuess = input("(guess): ") 
-            if(userGuess != randomPhrase[index]):
-                print("nope\n")
-            else:
-                print("correct\n")
-                index += 1
-                userStr += userGuess
-        else:
-            correctGuess = True
-            loop = False
+print("\n")
+
+solved = False
+#until user can guess the word
+while(solved == False):
+    print("Word - " + censoredPhrase)
+    userGuess = input("Guess: ")
+
+    
+    
+    
 
 
-print("nice ni=bba")
-exit()
-
-
+        
 
